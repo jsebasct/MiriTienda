0c3c5ab716d255c2efc1d8cd6f60d06835a48314
sebastian cruz <jsebas.ct@gmail.com>
2017-02-19 09:56:03 -0300

    chore: create skeleton project

---
+++ storemiri/src/main/java/org/store/miri/people/StoremiriApplication.java
@@ -0,0 +1,12 @@
+package org.store.miri.people;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoremiriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoremiriApplication.class, args);
+	}
+}
---
+++ storemiri/src/test/java/org/store/miri/people/StoremiriApplicationTests.java
@@ -0,0 +1,16 @@
+package org.store.miri.people;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StoremiriApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
